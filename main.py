8bb3d6ee96f23a1c3bc4d32fb8ba7cf326071ea0
Эвелина Мягдеева <evelinamagdeeva@MacBook-Pro-Evelina.local>
2025-11-29 17:43:41 +0300

    start1

--- main.py
+++ main.py
@@ -14,4 +14,6 @@ def about():
     return "<h1>О нас</h1><p>Это заглушка сайта на Flask.</p>"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
